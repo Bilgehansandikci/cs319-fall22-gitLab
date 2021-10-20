7a9f0bf07f0e3e46df13c7f3467af760e0d94236
Bilgehansandikci <bilgehansandikci@gmail.com>
2021-10-20 20:27:52 +0300

    refactor with f-strings

--- src/MaterialManager.py
+++ src/MaterialManager.py
@@ -5,28 +5,29 @@ class MaterialManager:
 
     def __init__(self, params: list):
         self.params = params
-        # other configurations
+      
     def changeMaterial(self, material) -> bool:
-        returnText = ""
-        textBaseForValidMaterial = "Material is changed to"
+    returnText = ""
+    textBaseForValidMaterial = "Material is changed to"
 
-        match material:
-            case materialType.Titanium:
-                self.carManager.setMaterial(materialType.Titanium)
-                returnText = textBaseForValidMaterial + " Titanium."
+    match material:
+        case materialType.Titanium:
+            self.carManager.setMaterial(materialType.Titanium)
+            returnText = f"{textBaseForValidMaterial} Titanium."
 
-            case materialType.Carbon:
-                self.carManager.setMaterial(materialType.Carbon)
-                returnText = textBaseForValidMaterial + " Carbon."
+        case materialType.Carbon:
+            self.carManager.setMaterial(materialType.Carbon)
+            returnText = f"{textBaseForValidMaterial} Carbon."
 
-            case materialType.AlloyX:
-                self.carManager.setMaterial(materialType.AlloyX)
-                returnText = textBaseForValidMaterial + " AlloyX."
+        case materialType.AlloyX:
+            self.carManager.setMaterial(materialType.AlloyX)
+            returnText = f"{textBaseForValidMaterial} AlloyX."
 
-            case _:
-                returnText = "Material selection is invalid."
+        case _:
+            returnText = "Material selection is invalid."
 
-        return self._announceMaterial(returnText)
+    return self._announceMaterial(returnText)
+
 
     def _announceMaterial(self, text: str) -> bool:
         returnValue = False
